3af997a4ec7f7cc99202c431bbeb2050edf89788
Alex Ogier <alex.ogier@gmail.com>
2010-11-24 23:52:34 -0500

    Made bootstrap a little more respectful.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
+link_file ()
+{
+    if [ -f ~/$1 ]; then
+        read -p "Clobber ~/$1 ? " yn
+        case $yn in
+            [Yy]* ) rm ~/$1; break;;
+            * ) echo "Skipping $1"; return;;
+        esac
+    fi
+    echo "~/$1 -> ~/dotfiles/home/$2"
+    ln -s ~/dotfiles/home/$2 ~/$1
+}
+
 # bash
 
-rm ~/.bash_profile
-rm ~/.bashrc
-
-ln -s ~/dotfiles/home/bash_profile ~/.bash_profile
-ln -s ~/dotfiles/home/bashrc ~/.bashrc
+link_file .bash_profile bash_profile
+link_file .bashrc bashrc
 
 # vim
 
-rm ~/.vimrc
-
-ln -s ~/dotfiles/home/vimrc ~/.vimrc
+link_file .vimrc vimrc
 
 # git
 
-rm ~/.gitconfig
-rm ~/.gitignore
-
-ln -s ~/dotfiles/home/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/home/gitignore ~/.gitignore
+link_file .gitconfig gitconfig
+link_file .gitignore gitignore
