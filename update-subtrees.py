5bb7e3343abc2d1bcf9861fb706597057fe6ac3c
Alex Ogier <alex.ogier@gmail.com>
2012-09-17 14:24:24 -0400

    Remove a bunch of old plugins that were merged into this repo

--- update-subtrees.py
+++ update-subtrees.py
@@ -10,18 +10,7 @@ The command may stop to let you manually resolve conflicts.
 import subprocess
 
 subtrees = [
-    ('ack.vim', 'http://github.com/mileszs/ack.vim', 'home/vim/bundle/ack.vim'),
-    ('ctrlp.vim', 'http://github.com/kien/ctrlp.vim', 'home/vim/bundle/ctrlp.vim'),
-    ('gundo.vim', 'http://github.com/sjl/gundo.vim', 'home/vim/bundle/gundo.vim'),
-    ('nerdcommenter', 'http://github.com/scrooloose/nerdcommenter', 'home/vim/bundle/nerdcommenter'),
-    ('nerdtree', 'http://github.com/scrooloose/nerdtree', 'home/vim/bundle/nerdtree'),
-    ('vim-pathogen', 'http://github.com/tpope/vim-pathogen.git', 'vim-pathogen'),
-    ('vim-scmdiff', 'http://github.com/ghewgill/vim-scmdiff', 'home/vim/bundle/vim-scmdiff/plugin'),
-    ('SearchComplete', 'http://github.com/vim-scripts/SearchComplete', 'home/vim/bundle/SearchComplete'),
-    ('snipmate.vim', 'http://github.com/msanders/snipmate.vim', 'home/vim/bundle/snipmate.vim'),
-    ('sudo.vim', 'http://github.com/vim-scripts/sudo.vim', 'home/vim/bundle/sudo.vim'),
-    ('vim-surround', 'http://github.com/tpope/vim-surround', 'home/vim/bundle/vim-surround'),
-    ('taglist.vim', 'http://github.com/vim-scripts/taglist.vim', 'home/vim/bundle/taglist.vim'),
+    ('vundle', 'http://github.com/gmarik/vundle', 'home/vim/bundle/vundle'),
 ]
 
 def run():
@@ -37,7 +26,9 @@ def run():
         else:
             subprocess.check_call('git fetch {0}'.format(remote), shell=True)
 
-        subprocess.check_call('git merge -s recursive -X subtree={0} {1}/master'.format(mount, remote), shell=True)
+        cmd = 'git merge -s recursive -X subtree={0} {1}/master'.format(mount, remote)
+        print cmd
+        subprocess.check_call(cmd, shell=True)
 
 if __name__ == '__main__':
     try:
